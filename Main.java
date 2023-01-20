3c9acdc2ba7009b47a284f50db4052883518195d
tekater <120014627+tekater@users.noreply.github.com>
2023-01-20 20:28:15 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Введите значение d (Давление): ");
+        double d = scanner.nextDouble(); // давление (1.7)
+        double g = 9.8; // 9.8 m/s^2 ускорение свободного падения
+        System.out.println("Введите значение h (Высота): ");
+        double h = scanner.nextDouble(); // высота (5.5)
+        double p = d * g * h; // плотность
+        System.out.println(p); // (91.63)
+
+    }
+}
